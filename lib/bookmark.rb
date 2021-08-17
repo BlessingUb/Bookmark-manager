fd3b2d23cbbf3d5ce11397fc4db25dfd70005f66
Blessing Ubogu <60205509+BlessingUb@users.noreply.github.com>
2021-08-17 17:44:30 +0100

    new commit

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,13 +1,25 @@
+<<<<<<< HEAD
 require 'pg'
 
+=======
+# in lib/bookmark.rb
+require 'pg'
+>>>>>>> 6069bf6 (new commit)
 class Bookmark
 
   def self.all
+<<<<<<< HEAD
     conn = PG.connect(dbname: 'bookmark_manager')
     result = conn.exec( 'SELECT * FROM bookmarks')
     result.map { |bookmarks|  bookmarks['url'] }
   end
 end
+=======
+    connection = PG.connect(dbname: 'bookmark_manager')
+    result = connection.exec('SELECT * FROM bookmarks')
+    result.map { |bookmark| bookmark['url'] }
+  end 
+>>>>>>> 6069bf6 (new commit)
 
 # puts Bookmark.new.all
 puts Bookmark.all
